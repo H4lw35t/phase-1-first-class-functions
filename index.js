8229919c919c7bdae9221f443682e2a7a4625792
Halwest Abubakir <halwest.east@gmail.com>
2024-02-28 17:32:09 +0300

    done

---
+++ index.js
@@ -0,0 +1,23 @@
+function receivesAFunction(func){
+    
+    return func()
+
+}
+
+
+function returnsANamedFunction() {
+    
+    function namedFunction() {
+       ;
+    }
+   
+    return namedFunction;
+}
+
+
+function returnsAnAnonymousFunction() {
+    
+    return function() {
+        
+    };
+}
